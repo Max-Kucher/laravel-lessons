df3092d31142d99f2342549cdd33f54370dff8bb
Max-Kucher <mkuchirov@gmail.com>
2023-02-05 12:41:59 +0200

    [n] Добавлены миграции по таблице товаров

---
+++ database/migrations/2023_02_05_102336_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product', 255);
+
+            $table->double('price', 12, 5);
+            $table->double('old_price', 12, 5)->nullable();
+
+            $table->boolean('in_stock');
+
+            $table->text('full_description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
