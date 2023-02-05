dacb425b0c08b4fe5049b9abcee2241572f6ae4d
Max-Kucher <mkuchirov@gmail.com>
2023-02-05 14:26:15 +0200

    [n] Добавлен сидер товаров

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = DB::table('products');
+
+        for ($i = 0; $i < 11; $i++) {
+            $p_id = $products->insertGetId([
+                'product' => sprintf('IPhone X (%d)', $i),
+                'price' => 100.00,
+                'old_price' => 120.00,
+                'in_stock' => true,
+                'full_description' => '<p>Full description</p>',
+                'short_description' => '<p>Short description</p>',
+            ]);
+
+            echo $p_id; // Тут новый id
+        }
+    }
+}
