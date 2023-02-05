a4c197573b146edc751ff6f3804abe63ab48b179
Max-Kucher <mkuchirov@gmail.com>
2023-02-05 15:45:10 +0200

    [n] Выборка товаров в index контроллере.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-class indexController extends Controller
+class IndexController extends Controller
 {
 
     /**
@@ -13,6 +14,12 @@ class indexController extends Controller
      */
     public function index() : View
     {
+        $products = Product::where('in_stock', true)->get();
+
+        foreach ($products as $product) {
+            echo $product->product . ' ' . $product->price . '<br/>';
+        }
+
         return view('index.index');
     }
 
