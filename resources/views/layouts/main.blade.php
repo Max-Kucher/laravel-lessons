d30bbb81975d00be9c4fdc1178b68aea4b954c3a
Max-Kucher <mkuchirov@gmail.com>
2023-02-09 10:51:41 +0200

    [n] Правки по мобильному меню.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -143,22 +143,25 @@
                     <li class="page_menu_item has-children menu_mm">
                         <a href="index.html">Home<i class="fa fa-angle-down"></i></a>
                         <ul class="page_menu_selection menu_mm">
-                            <li class="page_menu_item menu_mm"><a href="categories.html">Categories<i class="fa fa-angle-down"></i></a></li>
+                            @if (isset($menu_categories))
+                                <li class="page_menu_item menu_mm"><a href="{{ route('showCategory', [$menu_categories[0]->alias]) }}">{{ $menu_categories[0]->category }}<i class="fa fa-angle-down"></i></a></li>
+                            @endif
                             <li class="page_menu_item menu_mm"><a href="product.html">Product<i class="fa fa-angle-down"></i></a></li>
                             <li class="page_menu_item menu_mm"><a href="cart.html">Cart<i class="fa fa-angle-down"></i></a></li>
                             <li class="page_menu_item menu_mm"><a href="checkout.html">Checkout<i class="fa fa-angle-down"></i></a></li>
                             <li class="page_menu_item menu_mm"><a href="contact.html">Contact<i class="fa fa-angle-down"></i></a></li>
                         </ul>
                     </li>
-                    <li class="page_menu_item has-children menu_mm">
-                        <a href="categories.html">Categories<i class="fa fa-angle-down"></i></a>
-                        <ul class="page_menu_selection menu_mm">
-                            <li class="page_menu_item menu_mm"><a href="categories.html">Category<i class="fa fa-angle-down"></i></a></li>
-                            <li class="page_menu_item menu_mm"><a href="categories.html">Category<i class="fa fa-angle-down"></i></a></li>
-                            <li class="page_menu_item menu_mm"><a href="categories.html">Category<i class="fa fa-angle-down"></i></a></li>
-                            <li class="page_menu_item menu_mm"><a href="categories.html">Category<i class="fa fa-angle-down"></i></a></li>
-                        </ul>
-                    </li>
+                    @if (isset($menu_categories))
+                        <li class="page_menu_item has-children menu_mm">
+                            <a href="{{ route('showCategory', [$menu_categories[0]->alias]) }}">{{ $menu_categories[0]->category }}<i class="fa fa-angle-down"></i></a>
+                            <ul class="page_menu_selection menu_mm">
+                                @foreach($menu_categories as $category)
+                                    <li class="page_menu_item menu_mm"><a href="{{ route('showCategory', [$category->alias]) }}">{{ $category->category }}<i class="fa fa-angle-down"></i></a></li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
                     <li class="page_menu_item menu_mm"><a href="index.html">Accessories<i class="fa fa-angle-down"></i></a></li>
                     <li class="page_menu_item menu_mm"><a href="#">Offers<i class="fa fa-angle-down"></i></a></li>
                     <li class="page_menu_item menu_mm"><a href="contact.html">Contact<i class="fa fa-angle-down"></i></a></li>
